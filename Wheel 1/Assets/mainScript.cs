c182564c759f1999e0a92aa5f90333ace0d2b474
UgoFSofia <ugofsofia.universita@gmail.com>
2020-02-21 15:28:04 +0100

    Primo commit di prova
    Ho inserito le immagini di ruota e base, e uno script per la rotazione della ruota.

---
+++ Wheel 1/Assets/mainScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainScript : MonoBehaviour
+{
+
+	public float Speed = 600;
+	public bool isSpin = true;
+
+	void Update(){
+
+		Rotate ();
+	}
+   
+	void Rotate(){
+
+		transform.Rotate(0,0,  - Speed*Time.deltaTime);
+
+		if(isSpin == false && Speed > 0)
+		{
+			Stop();
+
+		}
+	}
+
+	public void Stop(){
+
+		Speed --;
+		if (Speed <= 0)
+		{
+			Speed = 0;
+		}
+
+	}
+
+}
