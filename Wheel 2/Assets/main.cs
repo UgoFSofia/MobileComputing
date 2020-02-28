33d4e27535c9cddb84085cd25d61a938d4e7907f
UgoFSofia <ugofsofia.universita@gmail.com>
2020-02-28 13:27:10 +0100

    Ignore

---
+++ Wheel 2/Assets/main.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    public float speed = 600;
+    public bool isSpin = true;
+    public GameObject pointer;
+   
+
+    void Update()
+    {
+
+    	Rotate();
+        
+    }
+
+    void Rotate()
+    {
+    		transform.Rotate(0,0, 0-speed*Time.deltaTime);
+
+    		if( isSpin==false && speed > 0)
+    		{
+    			Stop();
+    		}
+
+
+    }
+
+    public void Stop()
+    {
+    	speed --;
+
+    	if (speed<=0)
+    	{
+    		pointer.GetComponent<BoxCollider> ().enabled = true;
+    		speed = 0;
+    	}
+    }
+}
